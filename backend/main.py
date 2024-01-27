d5cb62316800d1f66ce77a263187c4abcd25a504
ilmal <nils@u1.se>
2024-01-27 21:07:52 +0100

    adding stuff

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,60 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 from modules.import_stories import import_stories
 from modules.translate_story import translate_story
 
+stories = [
+    [
+        "https://americanliterature.com/childrens-stories/the-three-little-pigs",
+        "The three little pigs"
+    ],
+    [
+        "https://americanliterature.com/childrens-stories/little-red-riding-hood",
+        "Little Red Riding Hood"
+    ],
+]
+
+languages = [
+    "zh-cn"
+]
+
+@app.route("/api/get_languages", methods=["GET"])
+def get_languages():
+    return languages
+
+@app.route("/api/get_stories", methods=["GET"])
+def get_stories():
+    return stories
+
+@app.route("/api/get_story_text", methods=["GET"])
+def get_story_text():
+    if not request.args.get('story', '') or not request.args.get('language', ''): 
+        return "request was without story or language info", 400
+        raise Exception("request did not have story or language info!")
+    story = request.args.get('story', '')
+    language = request.args.get('language', '')
+    
+    for e in stories:
+        if str(story) == str(e[1]):
+            (story, story_name) = import_stories(stories[0])
+            story_lines = [e for e in story.split("\n") if not e == "" and not e == "\r"]
+
+            if language not in languages: return "language provided is not supported", 400
+
+            translation_arr = translate_story(story_lines, language)
+
+            print(translation_arr)
+            return translation_arr
+
+
+
+    return "The story provided was not found on the server", 400
+
 def main():
-
-    stories = [
-        [
-            "https://americanliterature.com/childrens-stories/the-three-little-pigs",
-            "The three little pigs"
-        ],
-        [
-            "https://americanliterature.com/childrens-stories/little-red-riding-hood",
-            "Little Red Riding Hood"
-        ],
-    ]
-
-
     (story, story_name) = import_stories(stories[0])
 
     story_lines = [e for e in story.split("\n") if not e == "" and not e == "\r"]
@@ -31,5 +69,5 @@ def main():
 
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
--- frontend/src/pages/story.js
+++ frontend/src/pages/story.js
@@ -1,23 +1,39 @@
 import { useEffect, useState } from "react"
+import  { Link } from 'react-router-dom'
 
 const Story = () => {
 
-    const stories, setStories = useState([])
+    const [stories, setStories] = useState([])
 
     useEffect(() => {
+        console.log("ENV: ", process.env.REACT_APP_BACKEND_URL)
+        const URL = process.env.REACT_APP_BACKEND_URL + "/api/get_stories"
+        const requestOptions = {
+            method: 'GET',
+        };
+        fetch(URL, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                setStories(data)
+            })
 
-    })
+    }, [])
+
+    // const redirect_to_story = (story_title)=>{
+    //     console.log("Hello!")
+    //     browserHistory.push(`/story?story=${story_title}`);
+    // }
 
     const list_stories = () => {
         const stories_arr = []
 
         stories.forEach(element => {
-            stories_arr.append()
+            stories_arr.push(
+                <Link to={`/story?story=${element}`} key={element}>{element[1]}</Link>
+            )
         });
 
-        return (
-
-        )
+        return stories_arr
     }
 
     return (
@@ -26,7 +42,7 @@ const Story = () => {
                 This is the story page!
             </p>
             <p>Stories:</p>
-            {stories.length == 0 ? null : list_stories}
+            {stories.length === 0 ? null : list_stories()}
         </>
     )
 
